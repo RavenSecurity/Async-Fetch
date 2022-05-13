4971526ecc86b7a42fb565ceb1c03a6216328a93
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-05-13 21:49:15 +0200

    Bonus : country

--- Exercise 2/script.js
+++ Exercise 2/script.js
@@ -1,9 +1,10 @@
 const input = document.querySelector("input")
+const select = document.querySelector("select")
 const button = document.querySelector("button");
 const div = document.querySelector("div");
 
 button.addEventListener("click", function() {
-  fetch("https://api.agify.io/?name=" + input.value)
+  fetch("https://api.agify.io/?name=" + input.value + "&country_id=" + select.value)
     .then(res => res.json())
     .then(data => {
         console.log(data)
